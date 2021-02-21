b4dd70776e58bb1b52640aadd3685f78c97a41ab
Varchasv Hoon <75484743+Overkill-IG@users.noreply.github.com>
2021-02-21 13:32:00 +0530

    Update vector.py

--- vector.py
+++ vector.py
@@ -1,27 +1,28 @@
-class vector:
+class Vector:
     def __init__(self, vec):
-        self.vec = vec
+       self.vec = vec
+    
     def __str__(self):
-        str1 = ""
+        str1 = "" 
         index = 0
         for i in self.vec:
-            str1 += f"{i}a{index} +"
-            index += 1
+            str1 += f" {i}a{index} +"
+            index +=1
         return str1[:-1]
-    
+
     def __add__(self, vec2):
         newList = []
         for i in range(len(self.vec)):
             newList.append(self.vec[i] + vec2.vec[i])
-        return vector(newList)
-
+        return Vector(newList)
+    
     def __mul__(self, vec2):
-        sum  = 0
+        sum = 0
         for i in range(len(self.vec)):
-            sum += self.vec[i] + vec2.vec[i]
+            sum += self.vec[i] * vec2.vec[i]
         return sum
 
-v1 = vector([1, 4, 6])
-v2 = vector([1, 6, 9])
+v1 = Vector([1, 4, 6])
+v2 = Vector([1, 6, 9])
 print(v1+v2)
 print(v1*v2)
