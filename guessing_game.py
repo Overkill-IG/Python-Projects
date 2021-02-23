5765beeac1099b5e1c995fb9af73f277e0133e85
Varchasv Hoon <75484743+Overkill-IG@users.noreply.github.com>
2021-02-23 07:07:45 -0800

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -2,17 +2,21 @@ import random
 randNumber = random.randint(1, 100)
 guesses = 0
 user_guess = None
-while(user_guess != randNumber):
-    user_guess = int(input("Enter your Guess: "))
+try:
+    while(user_guess != randNumber):
+        user_guess = int(input("Enter your Guess: "))
 
-    if user_guess == randNumber:
-        print("Congratulations you guessed the number")
-    else:
-        print("You are wrong, ", end="")
-        guesses += 1
-        if user_guess < randNumber:
-            print("Guess higher")
-        elif user_guess > randNumber:
-            print("Guess Lower")
+        if user_guess == randNumber:
+           print(f"Congratulations you guessed the number, the number was '{randNumber}'")
+        else:
+           print("You are wrong, ", end="")
+           guesses += 1
+           if user_guess < randNumber:
+               print("Guess higher")
+           elif user_guess > randNumber:
+               print("Guess Lower")
 
-print(f"You guessed the number in {guesses} number of guesses.")
+    print(f"You guessed the number in {guesses} number of guesses.")
+except ValueError  as e:
+    print("Please enter a valid value")
+    print(f"Error: {e}")
